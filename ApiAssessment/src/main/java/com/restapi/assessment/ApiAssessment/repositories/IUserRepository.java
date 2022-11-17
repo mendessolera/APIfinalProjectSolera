72867254d1e8a1ad6c3e67464b9d33915d54d04a
Cris-Sand <cristian.mihai@solera.com>
2022-11-17 10:35:26 +0100

    Added UserRepository and UserService

--- ApiAssessment/src/main/java/com/restapi/assessment/ApiAssessment/repositories/IUserRepository.java
+++ ApiAssessment/src/main/java/com/restapi/assessment/ApiAssessment/repositories/IUserRepository.java
@@ -1,5 +1,21 @@
 package com.restapi.assessment.ApiAssessment.repositories;
 
-public interface IUserRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.restapi.assessment.ApiAssessment.models.User;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User, Integer> {
+	
+	public List<User> findAll();
+	
+	public User findById(int userId);
+	
+	public User save(User user);
+	
+	public void deleteById(Long uId);
 
 }
--- ApiAssessment/src/main/java/com/restapi/assessment/ApiAssessment/services/UserService.java
+++ ApiAssessment/src/main/java/com/restapi/assessment/ApiAssessment/services/UserService.java
@@ -1,5 +1,43 @@
 package com.restapi.assessment.ApiAssessment.services;
 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.restapi.assessment.ApiAssessment.models.User;
+import com.restapi.assessment.ApiAssessment.repositories.IUserRepository;
+
+@Service
 public class UserService {
+	
+	private IUserRepository userRepository;
+	
+	@Autowired
+	public UserService(IUserRepository userRepository){
+		this.userRepository = userRepository;
+	}
+	
+		//@Transactional(readOnly = true)
+		public List<User> findAll() {
+			return userRepository.findAll();
+		}
+		
+		public User findById(int userId) {
+			
+			return userRepository.findById(userId);
+		}
+		
+		public User save(User user) {
+			
+			return userRepository.save(user);
+		}
+		
+		public void delete(int userId) {
+			
+			userRepository.deleteById(userId);
+		}
 
 }
