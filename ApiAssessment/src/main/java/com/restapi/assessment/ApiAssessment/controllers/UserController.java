708be3905beb771adc1f4b40d1a743d39fc66ee2
mendessolera <daniel.mendes@solera.com>
2022-11-17 13:06:29 +0100

    CrossOrigin added

--- ApiAssessment/src/main/java/com/restapi/assessment/ApiAssessment/controllers/UserController.java
+++ ApiAssessment/src/main/java/com/restapi/assessment/ApiAssessment/controllers/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
 public class UserController {
