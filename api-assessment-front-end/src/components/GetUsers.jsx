dbcdbe5d8c14fb9997f844629f6817033c8fe791
Cris-Sand <cristian.mihai@solera.com>
2022-11-17 15:54:46 +0100

    Added User front-end table

---
+++ api-assessment-front-end/src/components/GetUsers.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { useEffect, useState } from "react";
+
+import './getusers.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function GetUsers() {
+    const [users, setUsers] = useState([]);
+
+    const getData = () => {
+        fetch(`http://localhost:8080/api/users`)
+            .then(res => res.json())
+            .then(users => setUsers(users))
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+
+
+    return (
+        <div className="col-8 shadow-lg p-3 text-center rounded" style={{ marginLeft: "auto", marginRight: "auto", marginTop: "10em" }}>
+            <h2>LIST OF USERS</h2>
+            <table className="table table-striped table-bordered" id="myTable">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Phone Number</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((users) =>
+                        <tr key={users.userID}>
+                            <td>{users.userID}</td>
+                            <td>{users.firstName}</td>
+                            <td>{users.lastName}</td>
+                            <td>{users.phoneNumber}</td>
+                            <td>{users.email}</td>
+                            <td><button type="button" class="btn btn-danger">DELETE</button></td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default GetUsers
