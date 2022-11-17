132170fd96d09a4b0e77294a7cb3c93f061cfbb6
mendessolera <daniel.mendes@solera.com>
2022-11-17 10:02:54 +0100

    Setup Complete

---
+++ ApiAssessment/src/main/java/com/restapi/assessment/ApiAssessment/ApiAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.assessment.ApiAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAssessmentApplication.class, args);
+	}
+
+}
