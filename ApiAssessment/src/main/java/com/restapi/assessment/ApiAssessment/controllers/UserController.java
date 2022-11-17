b00a2f7dd9957f4f2b2bb05bb210e757b1345829
mendessolera <daniel.mendes@solera.com>
2022-11-17 12:11:38 +0100

    Added User Post method

--- ApiAssessment/src/main/java/com/restapi/assessment/ApiAssessment/controllers/UserController.java
+++ ApiAssessment/src/main/java/com/restapi/assessment/ApiAssessment/controllers/UserController.java
@@ -10,8 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,7 +47,21 @@ public class UserController {
             return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
     }
         return new ResponseEntity<Optional<DCUser>>(user, HttpStatus.OK);
-}
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<?> addNewDCUser(@Validated @RequestBody DCUser dcUser){
+        DCUser usernew = null;
+        Map<String, Object> response = new HashMap<>();
+        try{
+            usernew = userService.save(dcUser);
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+        }catch (DataAccessException e){
+            response.put("ERROR", "User Already Exists");
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 
 
 }
