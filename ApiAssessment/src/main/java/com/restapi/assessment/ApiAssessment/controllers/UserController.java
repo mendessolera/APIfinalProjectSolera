47c73baf9e2ba0f868b9f829c8ad754e20fdc373
mendessolera <daniel.mendes@solera.com>
2022-11-17 10:18:12 +0100

    Users Model done

--- ApiAssessment/src/main/java/com/restapi/assessment/ApiAssessment/controllers/UserController.java
+++ ApiAssessment/src/main/java/com/restapi/assessment/ApiAssessment/controllers/UserController.java
@@ -1,5 +1,92 @@
 package com.restapi.assessment.ApiAssessment.controllers;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.springframework.lang.NonNull;
+
+@Entity
+@Table(name = "users")
 public class UserController {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private int userID;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String phoneNumber;
+
+    @Column(unique = true)
+    @NonNull
+    private String email;
+
+    public UserController() {
+    }
+
+    public UserController(int userID, String firstName, String lastName, String phoneNumber,
+            @NonNull String email) {
+        this.userID = userID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public UserController(String firstName, String lastName, String phoneNumber, @NonNull String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @NonNull
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NonNull String email) {
+        this.email = email;
+    }
 }
